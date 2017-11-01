b006929070a41c3c89b8db0e44b00c40cedc4513
alexandriaware <ali.mercer92@gmail.com>
2017-11-01 14:59:05 -0400

    IT WORKED

--- FortuneTellerMVC/Controllers/CustomersController.cs
+++ FortuneTellerMVC/Controllers/CustomersController.cs
@@ -32,6 +32,100 @@ namespace FortuneTellerMVC.Controllers
             {
                 return HttpNotFound();
             }
+            
+
+            //begin fortune teller response
+            //retirement age
+            if ((customer.Age % 2) == 0)
+            {
+                ViewBag.RetirementAge = 12;
+
+            }
+            else
+            {
+               ViewBag.RetirementAge = 20;
+            }
+
+            //In the bank
+            if (customer.BirthMonth >= 1 && customer.BirthMonth <= 4)
+            {
+                ViewBag.BankMoney = 1000000;
+            }
+            else if (customer.BirthMonth >= 5 && customer.BirthMonth <= 8)
+            {
+                ViewBag.BankMoney = 1000;
+            }
+            else if (customer.BirthMonth >= 9 && customer.BirthMonth <= 12)
+            {
+                ViewBag.BankMoney = 1;
+            }
+            else
+            {
+                ViewBag.BankMoney = 0;
+            }
+
+            //Transportations!
+            if (customer.FavoriteColor == "red")
+            {
+                ViewBag.Transport = "Motorized Wheelchair";
+            }
+            else if (customer.FavoriteColor == "orange")
+            {
+                ViewBag.Transport = "Arabian Stallion";
+            }
+            else if (customer.FavoriteColor == "yellow")
+            {
+                ViewBag.Transport = "Ice Cream Truck";
+            }
+            else if (customer.FavoriteColor == "green")
+            {
+                ViewBag.Transport = "Skateboard";
+            }
+            else if (customer.FavoriteColor == "blue")
+            {
+                ViewBag.Transport = "Yacht";
+            }
+            else if (customer.FavoriteColor == "indigo")
+            {
+                ViewBag.Transport = "Hover Car";
+            }
+            else if (customer.FavoriteColor == "violet")
+            {
+                ViewBag.Transport = "Tesla Model S";
+            }
+            else
+                ViewBag.Transport = "pair of roller skates";
+
+
+            //Homecation
+            if (customer.NumberOfSiblings == 0)
+            {
+                ViewBag.Location = "Amsterdam";
+            }
+            else if (customer.NumberOfSiblings == 1)
+            {
+                ViewBag.Location = "New York";
+            }
+            else if (customer.NumberOfSiblings == 2)
+            {
+                ViewBag.Location = "London";
+            }
+            else if (customer.NumberOfSiblings == 3)
+            {
+                ViewBag.Location = "Dublin";
+            }
+            else if (customer.NumberOfSiblings > 3)
+            {
+                ViewBag.Location = "Orlando";
+            }
+            else
+            {
+                ViewBag.Location = "WWII Germany";
+            }
+
+
+
+
             return View(customer);
         }
 
